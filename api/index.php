32a0bd0542701ef263c4a9b4c2d6396e187f704e
xRdtya <noufalraditya068@gmail.com>
2026-06-03 21:06:58 +0800

    -

--- api/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Mengarahkan Vercel untuk membaca core Laravel di folder public
-require __DIR__ . '/../public/index.php';
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,17 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        \Illuminate\Support\Facades\Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
-            $event->extendSocialite('apple', \SocialiteProviders\Apple\Provider::class);
-        });
-
-        // TrustProxies(at: '*') di bootstrap/app.php sudah memproses header
-        // X-Forwarded-Host dan X-Forwarded-Proto dari ngrok, sehingga
-        // getSchemeAndHttpHost() mengembalikan URL ngrok yang benar.
-        $baseUrl = request()->getSchemeAndHttpHost();
-
-        if (str_contains($baseUrl, 'ngrok')) {
-            \Illuminate\Support\Facades\URL::forceRootUrl($baseUrl);
-        }
+        //
     }
 }
